31906a2fee5ffbf7a957b3cea0624d5b0271d3b4
colinfrederickson <110066636+colinfrederickson@users.noreply.github.com>
2023-07-19 11:36:52 -0600

    Update mini.ts
    - Success Message for submit action

--- src/mini.ts
+++ src/mini.ts
@@ -4,6 +4,9 @@ import { employeeValidations } from "./blueprints";
 import { simpleSpaceSetup } from "./plugins/simple.space.setup";
 import { employeeSheetMini } from "./blueprints/sheets/employees_mini";
 import generateIds from "./jobs/generateIds";
+import { FlatfileEvent } from "@flatfile/listener";
+import api from "@flatfile/api";
+import axios from "axios";
 
 /**
  * This default export is used by Flatfile to register event handlers for any
@@ -42,4 +45,63 @@ export default function (listener: FlatfileListener) {
 
   listener.use(generateIds);
   listener.use(recordHook("employees", employeeValidations));
+  listener.filter({ job: "workbook:review-data" }, (configure) => {
+    configure.on(
+      "job:ready",
+      async ({ context: { jobId, workbookId }, payload }: FlatfileEvent) => {
+        const { data: sheets } = await api.sheets.list({ workbookId });
+
+        const records: { [name: string]: any } = {};
+        for (const [index, element] of sheets.entries()) {
+          records[`Sheet[${index}]`] = await api.records.get(element.id);
+        }
+
+        try {
+          await api.jobs.ack(jobId, {
+            info: "Starting job to submit data!",
+            progress: 10,
+          });
+
+          console.log(JSON.stringify(records, null, 2));
+
+          const webhookReceiver =
+            process.env.WEBHOOK_SITE_URL ||
+            "https://webhook.site/c83648d4-bf0c-4bb1-acb7-9c170dad4388";
+
+          const response = await axios.post(
+            webhookReceiver,
+            {
+              ...payload,
+              method: "axios",
+              sheets,
+              records,
+            },
+            {
+              headers: {
+                "Content-Type": "application/json",
+              },
+            },
+          );
+
+          if (response.status === 200) {
+            await api.jobs.complete(jobId, {
+              outcome: {
+                message: "Data was successfully submitted. Go check it out!",
+              },
+            });
+          } else {
+            throw new Error("Failed to submit data!");
+          }
+        } catch (error) {
+          console.log(`webhook.site[error]: ${JSON.stringify(error, null, 2)}`);
+
+          await api.jobs.fail(jobId, {
+            outcome: {
+              message: "This job failed please reach out to support.",
+            },
+          });
+        }
+      },
+    );
+  });
 }
