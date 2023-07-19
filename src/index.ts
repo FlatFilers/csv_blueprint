d933bbbadc69254849a59f72c71700e0fde78a02
colinfrederickson <110066636+colinfrederickson@users.noreply.github.com>
2023-07-19 09:15:49 -0600

    Update index.ts
    - adds locations validations to index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,13 @@ import { FlatfileListener } from "@flatfile/listener";
 import { recordHook } from "@flatfile/plugin-record-hook";
 import { xlsxExtractorPlugin } from "@flatfile/plugin-xlsx-extractor";
 import { XMLExtractor } from "@flatfile/plugin-xml-extractor";
-import { companyValidations, employeeValidations } from "./blueprints";
+import { companyValidations, employeeValidations, locationValidations } from "./blueprints";
 import { configureSpace } from "./jobs/space:configure";
 import { seedCountries } from "./handlers/seed.countries";
 import reviewData from "./jobs/workbook:review-data";
 import watchList from "./jobs/watchlist";
 import { xlsxSinkPlugin } from "./jobs/export";
 import generateIds from "./jobs/generateIds";
-
 
 /**
  * This default export is used by Flatfile to register event handlers for any
@@ -26,6 +25,7 @@ export default function (listener: FlatfileListener) {
 
   listener.use(recordHook("companies", companyValidations));
   listener.use(recordHook("employees", employeeValidations));
+  listener.use(recordHook("locations", locationValidations));
 
   listener.use(xlsxExtractorPlugin());
   listener.use(XMLExtractor());
